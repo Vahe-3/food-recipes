3d8ab713e0c20682e50981858e2deeced85dbba5
Vahe-3 <vahe.avetisyan333@gmail.com>
2022-04-22 13:05:06 +0400

    add basename

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
 import {BrowserRouter} from "react-router-dom";
-import Nav from "./Components/Nav/Nav";
+
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename="/food-recipes">
                 <Header/>
 
                 <Main/>
